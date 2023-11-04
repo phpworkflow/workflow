78adc0704e4c93462bafb3553d33cd37d4f45579
Ruslan Dmytrakovych <ruslan.dmytrakovych@cashberry.com.ua>
2023-11-04 17:12:25 +0200

    Add workflow type to event

--- src/Storage/Redis/Event.php
+++ src/Storage/Redis/Event.php
@@ -6,13 +6,16 @@ class Event
 {
     protected int $workflow_id;
 
+    protected ?string $workflow_type;
+
     protected string $created_at;
 
     protected ?int $scheduled_at = null;
 
-    public function __construct(int $workflow_id, ?int $scheduled_at = null, string $created_at = null)
+    public function __construct(int $workflow_id, ?string $workflow_type = null, ?int $scheduled_at = null, string $created_at = null)
     {
         $this->workflow_id = $workflow_id;
+        $this->workflow_type = $workflow_type;
         $this->scheduled_at = $scheduled_at;
         $this->created_at = $created_at ?? date('Y-m-d H:i:s', time());
     }
@@ -21,6 +24,7 @@ class Event
     {
         return [
             'workflow_id' => $this->workflow_id,
+            'workflow_type' => $this->workflow_type,
             'created_at' => $this->created_at,
             'scheduled_at' => $this->scheduled_at
         ];
@@ -28,9 +32,9 @@ class Event
 
     public function fromArray(array $data): self
     {
-        $this->workflow_id = $data['workflow_id'] ?? 0;
-        $this->created_at = $data['created_at'] ?? '';
-        $this->scheduled_at = $data['scheduled_at'] ?? 0;
+        $this->workflow_id = (int)$data['workflow_id'] ?? $this->workflow_id;
+        $this->workflow_type = $data['workflow_type'] ?? $this->workflow_type;
+        $this->scheduled_at = (int)$data['scheduled_at'] ?? $this->scheduled_at;
 
         return $this;
     }
@@ -43,5 +47,10 @@ class Event
     public function getScheduledAt(): ?int
     {
         return $this->scheduled_at;
+    }
+
+    public function getWorkflowType(): ?string
+    {
+        return $this->workflow_type;
     }
 }
