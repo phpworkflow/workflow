335d07ca2aa0a3f584526de930676125ebeaaa68
Ruslan Dmytrakovych <ruslan.dmytrakovych@cashberry.com.ua>
2023-11-17 11:00:37 +0200

    Add lock object

---
+++ src/Storage/Redis/Lock.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Workflow\Storage\Redis;
+use Redis;
+
+class Lock
+{
+    protected const DEFAULT_KEY_EXPIRE_TIME = 30;
+
+    protected Connection $connection;
+
+    protected string $name;
+
+    protected string $value;
+
+    protected int $expire;
+
+    public function __construct(string $name, string $value, int $expire = self::DEFAULT_KEY_EXPIRE_TIME)
+    {
+        $this->name = $name;
+        $this->value = $value;
+        $this->expire = $expire;
+    }
+
+    public function lock(): bool
+    {
+
+        if (!$this->redis()->setnx($this->name, $this->value)) {
+            return false;
+        }
+
+        if (!$this->redis()->expire($this->name, $this->expire)) {
+            $this->redis()->del($this->name);
+            return false;
+        }
+
+        return true;
+    }
+
+    public function isLocked(): bool
+    {
+        $lockValue = $this->redis()->get($this->name);
+
+        if ($lockValue !== $this->value) {
+            return false;
+        }
+
+        if (!$this->redis()->expire($this->name, $this->expire)) {
+            return false;
+        }
+
+        $lockValue = $this->redis()->get($this->name);
+
+        return ($lockValue === $this->value);
+    }
+
+    public function unlock(): bool
+    {
+        return ($this->redis()->del($this->name) > 0);
+    }
+
+    protected function redis(): Redis
+    {
+        if(empty($this->connection)) {
+            $this->connection = new Connection(new Config());
+        }
+
+        return $this->connection->connection();
+    }
+}
